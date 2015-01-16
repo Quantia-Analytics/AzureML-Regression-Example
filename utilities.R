f723fa819bdf2c9f9e3cc2babb8e93dc34809c06
stephen.elston <stephen.elston@quantia.com>
2015-01-16 08:57:57 -0800

    Some format changes to improve readability.

--- utilities.R
+++ utilities.R
@@ -1,35 +1,47 @@
-## This code contains some utility functions used in several Execute R Script
-## modules. This file should be zipped and uploaded as a dataset into Azure ML
-## Studio. Each Execute R Script model which uses these utilities imports them
-## using the R source() function. 
+## This code contains some utility functions used in 
+## several Execute R Script modules. This file should 
+## be zipped and uploaded as a dataset into Azure ML
+## Studio. Each Execute R Script model which uses 
+## these utilities imports them using the R source() 
+## function. 
 
 
 fact.conv <- function(inVec){
+  ## Function gives the day variable meaningful 
+  ## level names.
   outVec <- as.factor(inVec)
-  levels(outVec) <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+  levels(outVec) <- c("Monday", "Tuesday", "Wednesday", 
+                      "Thursday", "Friday", "Saturday", 
+                      "Sunday")
   outVec
 }
 
 
 get.date <- function(Date){
-  ## Funciton returns the data as a character string from a POSIXct datatime object. 
+  ## Funciton returns the data as a character 
+  ## string from a POSIXct datatime object. 
   strftime(Date, format = "%Y-%m-%d %H:%M:%S")
 }
 
 
 POSIX.date <- function(Date,Hour){
-  ## Function returns POSIXct time series object from date and hour arguments.
-  as.POSIXct(strptime(paste(Date, " ", as.character(Hour), ":00:00", sep = ""), "%Y-%m-%d %H:%M:%S"))
+  ## Function returns POSIXct time series object 
+  ## from date and hour arguments.
+  as.POSIXct(strptime(paste(Date, " ", as.character(Hour), 
+                            ":00:00", sep = ""), 
+                        "%Y-%m-%d %H:%M:%S"))
 }
 
 
 serList <- function(serlist){
-  ## Function to serialize list of R objects and returns a dataframe.
-  ## The argument is a list of R objects. 
-  ## The function returns a serialized list with two elements.
-  ## The first element is count of the elements in the input list.
-  ## The second element, called payload, containts the input list.
-  ## If the serialization fails, the first element will have a value of 0,
+  ## Function to serialize list of R objects and 
+  ## returns a dataframe. The argument is a list 
+  ## of R objects. The function returns a serialized 
+  ## list with two elements. The first element is 
+  ## count of the elements in the input list.
+  ## The second element, called payload, containts 
+  ## the input list. If the serialization fails, 
+  ## the first element will have a value of 0,
   ## and the payload will be NA.
   
   ## Messages to use in case an error is encountered.
@@ -38,20 +50,29 @@ serList <- function(serlist){
                  "The serialization has failed in function serList")
   
   ## Check the input list for obvious problems
-  if(!is.list(serlist) | is.null(serlist) | length(serlist) < 1) {
+  if(!is.list(serlist) | is.null(serlist) | 
+       length(serlist) < 1) {
     warning(messages[2])
-    return(data.frame(as.integer(serialize(list(numElements = 0, payload = NA), connection = NULL))))}
+    return(data.frame(as.integer(serialize(
+        list(numElements = 0, payload = NA), 
+      connection = NULL))))}
   
   ## Find the number of objects in the input list.
   nObj  <-  length(serlist)
   
   ## Serialize the output list and return a data frame.
-  ## The serialization and assignment are wrapped in tryCatch
-  ## in case anything goes wrong. 
-  tryCatch(outframe <- data.frame(payload = as.integer(serialize(list(numElements = nObj, payload = serlist), connection=NULL))),
-           error = function(e){warning(messages[3])
-                               outframe <- data.frame(payload = as.integer(serialize(list(numElements = 0, payload = NA), connection=NULL)))}
-  )
+  ## The serialization and assignment are wrapped in 
+  ## tryCatch in case anything goes wrong. 
+  tryCatch(outframe <- data.frame(payload = as.integer(
+        serialize(list(numElements = nObj, 
+                   payload = serlist), 
+          connection=NULL))),
+      error = function(e){warning(messages[3])
+        outframe <- data.frame(
+          payload = as.integer(serialize(list(
+           numElements = 0, payload = NA), 
+           connection=NULL)))}
+    )
   outframe
 }
 
@@ -69,19 +90,22 @@ unserList <- function(inlist){
                 "Function unserList has encountered an empty list")
   
   ## Check the input type.
-  if(!is.integer(inlist$payload) | dim(inlist)[1] < 2 | is.null(inlist$payload)){
+  if(!is.integer(inlist$payload) | dim(inlist)[1] < 2 | 
+       is.null(inlist$payload)){
     warning(messages[1]) 
     return(NA)
   }
   
-  ## Unserialized the list. The unserialize and assignment are
-  ## wrapped in tryCatch in case something goes wrong. 
+  ## Unserialized the list. The unserialize and 
+  ## assignment are wrapped in tryCatch in case something
+  ##  goes wrong. 
   tryCatch(outList <- unserialize(as.raw(inlist$payload)),
            error = function(e){warning(messages[2]); return(NA)})
   
   ## Check if the list is empty, which indicates something went 
   ## wrong with the serialization provess.
-  if(outList$numElements < 1 ) {warning(messages[3]); return(NA)}
+  if(outList$numElements < 1 ) {warning(messages[3]); 
+                                return(NA)}
   
   outList$payload
 }
