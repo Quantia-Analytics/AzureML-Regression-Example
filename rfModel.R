cabb4f3a87569706abf3f7d3af5602e1d60d3a34
stephen.elston <stephen.elston@quantia.com>
2015-01-16 08:57:38 -0800

    Some format changes to improve readability.

--- rfModel.R
+++ rfModel.R
@@ -1,7 +1,7 @@
 ## This code computes a random forest model.
-## This code is s intended to run in an Azure ML Execute R 
-## Script module. It can be tested in RStudio by now executing the Azure ML
-## specific code.
+## This code is s intended to run in an Azure ML 
+## Execute R Script module. It can be tested in 
+## RStudio by now executing the Azure ML specific code.
 
 ## Source the zipped utility file
 source("src/utilities.R")
@@ -10,8 +10,11 @@ source("src/utilities.R")
 BikeShare <- maml.mapInputPort(1)
 
 library(randomForest)
-rf.bike <- randomForest(cnt ~ xformHr + temp + monthCount + hum + dayWeek + mnth + isWorking + workTime, 
-                        data = BikeShare, ntree = 500, importance = TRUE, nodesize = 25)
+rf.bike <- randomForest(cnt ~ xformHr + temp + 
+                          monthCount + hum + dayWeek + 
+                          mnth + isWorking + workTime, 
+                        data = BikeShare, ntree = 500, 
+                        importance = TRUE, nodesize = 25)
 
 importance(rf.bike)
 
